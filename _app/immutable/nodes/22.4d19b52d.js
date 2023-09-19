df3f3c4b386c13ec3652069b64e8f15308cafa7b
antopiahk <31695089+antopiahk@users.noreply.github.com>
2023-09-19 13:57:12 +0000

    Deploying to gh-pages from @ Futino/futino@8e9610e1593b368e7086377c0350a22a4520ffac 🚀

---
+++ _app/immutable/nodes/22.4d19b52d.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.b428cfaa.js";import{S as t,i as e}from"../chunks/index.3e2d52a7.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
